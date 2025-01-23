270439d40a781a88726b9f6bacba60225457c553
SnakyDH <skdanielg5@gmail.com>
2025-01-23 03:24:38 -0500

    feat(breed_detail): add text for breed detail

--- lib/config/theme/app_font_theme.dart
+++ lib/config/theme/app_font_theme.dart
@@ -5,16 +5,10 @@ class AppFontsConstants {
   static final displayLarge = GoogleFonts.pacifico();
   static final displayMedium = GoogleFonts.pacifico();
   static final displaySmall = GoogleFonts.pacifico();
-  static final titleLarge = GoogleFonts.pacifico();
-  static final titleMedium = GoogleFonts.pacifico();
-  static final titleSmall = GoogleFonts.pacifico();
 }
 
 TextTheme appFontTheme = GoogleFonts.openSansTextTheme().copyWith(
   displayLarge: AppFontsConstants.displayLarge,
   displayMedium: AppFontsConstants.displayMedium,
   displaySmall: AppFontsConstants.displaySmall,
-  titleLarge: AppFontsConstants.titleLarge,
-  titleMedium: AppFontsConstants.titleMedium,
-  titleSmall: AppFontsConstants.titleSmall,
 );
